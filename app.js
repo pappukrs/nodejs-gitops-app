9b361272a790cf45cee38683a21581dd55b167f8
pappukrs <digitalpappu101@gmail.com>
2025-10-23 19:06:53 +0530

    chneges

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello GitOps");
+  res.send("Hello GitOps changes");
 });
 
 const PORT = process.env.PORT || 3000;
