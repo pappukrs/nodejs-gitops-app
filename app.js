5c4f984900245535912bbfe9e5e2e1d141b8ba52
pappukrs <digitalpappu101@gmail.com>
2025-10-24 04:23:41 +0530

    argo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello GitOps changes again");
+  res.send("Hello GitOps from argo");
 });
 
 const PORT = process.env.PORT || 3000;
