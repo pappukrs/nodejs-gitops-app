cacfdc85e7527ab0ef8f57eb2a7051dc9d5260fa
pappukrs <digitalpappu101@gmail.com>
2025-10-24 05:11:46 +0530

    workflow changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello GitOps from argo changes again");
+  res.send("Hello GitOps from argo changes again changes");
 });
 
 const PORT = process.env.PORT || 3000;
